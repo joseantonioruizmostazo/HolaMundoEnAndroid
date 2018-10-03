f30bd2f20b5535a6ecfc54de55b83d1f902fa17e
joseantonioruizmostazo <jaruizmostazo@gmail.com>
2018-10-03 11:06:46 +0200

    Hola mundo en android

---
+++ app/src/main/java/com/example/joseruiz/holamundo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.joseruiz.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView msg ;
+    private Button btn ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        msg = (TextView) findViewById(R.id.textView) ;
+        btn = (Button) findViewById(R.id.button) ;
+
+        //msg.setText("Me han pulsado!") ; 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            msg.setText(R.string.texto_pulsado) ;
+            }
+        });
+
+    }
+}
